f5181cddb4602681efad650a6521638d3debce54
sAmH365 <npower5377@naver.com>
2024-03-11 16:49:40 +0900

    검색기능 1

--- server.js
+++ server.js
@@ -229,3 +229,9 @@ app.post('/register', async(req, res) => {
 })
 
 app.use('/shop', require('./routes/shop.js'))
+
+app.get('/search', async (req, res) => {
+    let result = await db.collection('post')
+        .find({title : { $regex : req.query.val}}).toArray()
+    res.render('search.ejs', {posts: result})
+})
